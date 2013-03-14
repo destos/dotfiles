d9341ce5c62f8cbad4520ac6ecb3908eaa1703c5
Patrick Forringer <patrick@forringer.com>
2013-03-13 22:10:56 -0500

    Changing default osx editor to mate, and ubuntu to vim

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,19 +1,19 @@
 # Editing
 
 if [[ ! "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
-  export EDITOR='subl -w'
-  export LESSEDIT='subl %f'
-  alias q='subl'
+  export EDITOR='mate -w'
+  export LESSEDIT='mate %f'
+  alias m='mate'
 else
-  export EDITOR=$(type nano pico vi vim 2>/dev/null | sed 's/ .*$//;q')
-  alias q="$EDITOR -w -z"
+  export EDITOR=$(which vi vim nano pico 2>/dev/null | sed 's/ .*$//;q')
+  alias e="$EDITOR -w -z"
 fi
 
 export VISUAL="$EDITOR"
 
-alias q.='q .'
+alias m.='m .'
 
 function qs() {
   pwd | perl -ne"s#^$(echo ~/.dotfiles)## && exit 1" && cd ~/.dotfiles
-  q ~/.dotfiles
+  m ~/.dotfiles
 }
