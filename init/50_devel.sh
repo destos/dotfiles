912901fbde972c84a7d1ff23a29c30a5076d1bbf
Patrick Forringer <patrick@forringer.com>
2013-03-13 22:05:28 -0500

    removing ruby devlopment utils

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -30,28 +30,3 @@ if [[ "$(type -P npm)" ]]; then
     npm install -g $list
   fi
 fi
-
-# Install Ruby.
-if [[ "$(type -P rbenv)" ]]; then
-  versions=(1.9.3-p194 1.9.2-p290)
-
-  list="$(to_install "${versions[*]}" "$(rbenv whence ruby)")"
-  if [[ "$list" ]]; then
-    e_header "Installing Ruby versions: $list"
-    for version in $list; do rbenv install "$version"; done
-    [[ "$(echo "$list" | grep -w "${versions[0]}")" ]] && rbenv global "${versions[0]}"
-    rbenv rehash
-  fi
-fi
-
-# Install Gems.
-if [[ "$(type -P gem)" ]]; then
-  gems=(bundler awesome_print interactive_editor)
-
-  list="$(to_install "${gems[*]}" "$(gem list | awk '{print $1}')")"
-  if [[ "$list" ]]; then
-    e_header "Installing Ruby gems: $list"
-    gem install $list
-  fi
-fi
-
