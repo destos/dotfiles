b9fc73b0d9215830c7ac35e1dae4b8ec2d85ea5e
Patrick Forringer <patrick@forringer.com>
2014-09-19 13:26:45 -0500

    more cask apps

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -27,7 +27,7 @@ if [[ "$(which brew)" ]]; then
     brew install $list
   fi
 
-  cask_apps=(launchrocket alfred asepsis qlstephen vagrant virtualbox google-chrome iterm2)
+  cask_apps=(launchrocket alfred asepsis qlstephen vagrant virtualbox google-chrome iterm2 handbrake shortcat firefox steam onepassword vlc beamer seil mailbox witch)
   cask_list="$(to_install "${cask_apps[*]}" "$(brew cask list)")"
   if [[ "$cask_list" ]]; then
     e_header "Installing Applications via cask : $cask_list"
